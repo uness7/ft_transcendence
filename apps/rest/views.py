fb73e368f06a6f03f05cda0d04cc784d337a435e
liguyon <lguyon98@gmail.com>
2024-08-20 05:29:00 +0200

    feat: partial implementation of custom user model

--- apps/rest/views.py
+++ apps/rest/views.py
@@ -1,14 +1,10 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import permissions, viewsets
-from .serializers import GroupSerializer, UserSerializer
+from .serializers import UserSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
