21b05017e83be5dfbb8e792887bd4f748fc32219
liguyon <lguyon98@gmail.com>
2024-08-20 05:29:00 +0200

    feat: partial implementation of custom user model

--- apps/rest/urls.py
+++ apps/rest/urls.py
@@ -1,10 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import GroupViewSet, UserViewSet
+from .views import UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
-router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
