a11de392e383ebe0989c4a77f6a5661c696ce892
liguyon <lguyon98@gmail.com>
2024-08-30 14:22:12 +0200

    refac: finish moving backend into its own folder

--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("apps.rest.urls")),
 ]
