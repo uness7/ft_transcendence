87338f8da9358da630bb9d7e046ac7deb72c5361
liguyon <lguyon98@gmail.com>
2024-09-24 17:17:38 +0200

    feat: rectangle class

---
+++ pong/maths/vec2.js
@@ -0,0 +1,31 @@
+export default class Vec2 {
+	// TODO?: set fields to private, implement accessors
+	constructor(x=0, y=0) {
+		this.x = x;
+		this.y = y;
+	}
+
+	add = (v) => {
+		this.x += v.x;
+		this.y += v.y;
+	}
+
+	// scalaire
+	mul = (k) => {
+		this.x *= k;
+		this.y *= k;
+	}
+
+	newMul = (k) => {
+		return new Vec2(this.x * k, this.y * k);
+	}
+
+	clone = () => {
+		return new Vec2(this.x, this.y);
+	}
+
+	set = (v) => {
+		this.x = v.x;
+		this.y = v.y;
+	}
+}
