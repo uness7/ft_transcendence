bac37d5354fd886702f0aeab41f53d13e1c8b800
liguyon <lguyon98@gmail.com>
2024-09-24 09:06:50 +0200

    refac: split into modules

---
+++ pong/canvas.js
@@ -0,0 +1,6 @@
+// globals
+export const canvas = document.querySelector("#game-canvas")
+export const ctx = canvas.getContext("2d");
+export const aspectRatio = 16 / 9;
+export const canvasWidth = 1280;
+export const canvasHeight = canvasWidth / aspectRatio;
---
+++ pong/vec2.js
@@ -0,0 +1,7 @@
+export default class Vec2 {
+	// TODO?: set fields to private, implement accessors
+	constructor(x=0, y=0) {
+		this.x = x;
+		this.y = y;
+	}
+}
