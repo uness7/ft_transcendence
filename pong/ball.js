d79d185f5e3300685c3bf135203395cb47a68398
liguyon <lguyon98@gmail.com>
2024-09-24 17:41:18 +0200

    refac: cleaner ball class

--- pong/ball.js
+++ pong/ball.js
@@ -1,37 +1,26 @@
-import Vec2 from "./vec2.js";
+import Vec2 from "./maths/vec2.js";
 import { ctx, canvasWidth, canvasHeight } from "./canvas.js";
 
 
 export default class Ball {
-	#position;
-	#radius;
-	#color;
-
 	constructor(radius=8, color="white") {
-		this.#radius = radius;
-		this.#color = color;
-		this.startSpeed = new Vec2(-370, 0);
-		this.speed = this.startSpeed.clone();
+		this.color = color;
+		this.radius = radius;
+		this.startSpeed = new Vec2(370, 0);
+		this.speed = {...this.startSpeed};
 		this.startPosition = new Vec2(canvasWidth / 2, canvasHeight / 2);
-		this.#position = new Vec2();
-	}
-
-	get position() {
-		return this.#position;
-	}
-
-	get radius() {
-		return this.#radius;
+		this.position = {...this.startPosition};
 	}
 
 	reset = () => {
 		this.position.set(this.startPosition);
+		this.speed.set(this.startSpeed);
 	}
 
 	render = () => {
-		ctx.fillStyle = this.#color;
+		ctx.fillStyle = this.color;
 		ctx.beginPath();
-		ctx.arc(this.#position.x, this.#position.y, this.radius, 0, 2 * Math.PI);
+		ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
 		ctx.fill();
 		ctx.closePath();
 	}
