4f6de98ebdb7204e0fb3315ed35afa79b6136040
liguyon <lguyon98@gmail.com>
2024-09-24 09:09:11 +0200

    refac: move Ball class into its own module

---
+++ pong/ball.js
@@ -0,0 +1,33 @@
+import Vec2 from "./vec2.js";
+import { ctx, canvasWidth, canvasHeight } from "./canvas.js";
+
+
+export default class Ball {
+	#position;
+	#radius;
+	#color;
+
+	constructor(radius=10, color="white") {
+		this.#position = new Vec2(canvasWidth / 2, canvasHeight / 2);
+		this.#radius = radius;
+		this.#color = color;
+	}
+
+	get position() {
+		return this.#position;
+	}
+
+	get radius() {
+		return this.#radius;
+	}
+
+	moveToCenter() {
+		this.#position.setCoordinates(canvasWidth / 2, canvasHeight / 2);
+	}
+
+	render() {
+		ctx.fillStyle = this.#color;
+		ctx.arc(this.#position.x, this.#position.y, this.radius, 0, 2 * Math.PI);
+		ctx.fill();
+	}
+}
