4af2e24e4e87cf662a5e8a420033959ef7e09ff3
liguyon <lguyon98@gmail.com>
2024-09-24 06:36:02 +0200

    feat: basic paddle implementation

--- pong/index.js
+++ pong/index.js
@@ -9,12 +9,64 @@ const canvasHeight = canvasWidth / aspectRatio;
 canvas.setAttribute("width", canvasWidth);
 canvas.setAttribute("height", canvasHeight);
 
+
+class Vec2 {
+	// TODO?: set fields to private, implement accessors
+	constructor(x=0, y=0) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class Paddle {
+	#position;
+	#width;
+	#height;
+	#color;
+
+	constructor(width, height, padding, side="left", color="white") {
+		this.#width = width; 
+		this.#height = height;
+		this.#color = color;
+
+		let x = 0;
+		if (side === "left")
+				x = padding;
+		else if (side === "right")
+				x = canvasWidth - padding - this.#width;
+		else
+			throw new Error(`Invalid argument: side must be 'left' or 'right'`);
+		this.#position = new Vec2(x, (canvasHeight / 2) - (this.#height / 2));
+	}
+
+	get position() {
+		return this.#position;
+	}
+
+	get width() {
+		return this.#width;
+	}
+
+	get height() {
+		return  this.#height;
+	}
+
+	render(ctx) {
+		ctx.fillStyle = this.#color;
+		ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+	}
+}
+
+
+
 const render = () => {
 	if (canvas.getContext) {
 		const ctx = canvas.getContext("2d");
 
-		ctx.fillStyle = "white";
-		ctx.fillRect(10, 10, 100, 100);
+		paddleLeft = new Paddle(15, 100, 20);
+		paddleLeft.render(ctx);
+		paddleRight = new Paddle(15, 100, 20, "right");
+		paddleRight.render(ctx);
 	}
 }
 
