a0b74fa300344f7683acdccf1c9df41e61edbe9b
liguyon <lguyon98@gmail.com>
2024-09-24 11:28:38 +0200

    fix: ball not getting cleared at each frame

--- pong/ball.js
+++ pong/ball.js
@@ -27,7 +27,9 @@ export default class Ball {
 
 	render = () => {
 		ctx.fillStyle = this.#color;
+		ctx.beginPath();
 		ctx.arc(this.#position.x, this.#position.y, this.radius, 0, 2 * Math.PI);
 		ctx.fill();
+		ctx.closePath();
 	}
 }
