c9c1cae464df88874a24ac571332eaff16dd8ea8
liguyon <lguyon98@gmail.com>
2024-09-25 04:57:39 +0200

    feat: TextHUD remove private attributes

--- pong/entities/text-hud.js
+++ pong/entities/text-hud.js
@@ -2,26 +2,17 @@ import { ctx } from "../misc/canvas.js";
 
 
 export default class TextHUD {
-	#color;
-	#font;
-	#text;
-	#position
-
 	constructor(text, position, font="48px serif", color="white") {
-		this.#color = color;
-		this.#font = font;
-		this.#position = position;
+		this.color = color;
+		this.font = font;
+		this.position = position;
 		this.text = text;
 	}
 
-	set text(newText) {
-		this.#text = newText;
-	}
-
 	render = () => {
-		ctx.fillStyle = this.#color;
-		ctx.font = this.#font;
+		ctx.fillStyle = this.color;
+		ctx.font = this.font;
 		ctx.textAlign = "center";
-		ctx.fillText(this.#text, this.#position.x, this.#position.y);
+		ctx.fillText(this.text, this.position.x, this.position.y);
 	}
 }
