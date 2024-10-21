8672bc234f172170e319ec1b0262b5eece0fbc7b
Youness Zioual <crness2@gmail.com>
2024-10-21 14:44:47 +0200

    mod: changed the expiration date for access_token

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -82,7 +82,7 @@ CORS_ALLOW_HEADERS = (
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=3),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=45),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
